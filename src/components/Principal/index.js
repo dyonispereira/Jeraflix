92febf97906ec16fb7a7babc7eda62aca690d469
Dyonis Pereira <88103391+dyonispereira@users.noreply.github.com>
2022-05-22 20:09:30 -0400

    Update index.js

--- src/components/Principal/index.js
+++ src/components/Principal/index.js
@@ -94,20 +94,21 @@ export default ()=> {
 
 </footer>
 
-{movieList.length <= 0 &&
-      <div className="loading">
-        <img src="https://cdn.lowgif.com/small/0534e2a412eeb281-the-counterintuitive-tech-behind-netflix-s-worldwide.gif" alt="loading"></img>
-      </div>
-    }
+
     </div>
 
 
   );  
 }
 
-//export default App;
 
 
+/*export default App;
+{movieList.length <= 0 &&
+      <div className="loading">
+      <img src="https://cdn.lowgif.com/small/0534e2a412eeb281-the-counterintuitive-tech-behind-netflix-s-worldwide.gif" alt="loading"></img>
+    </div>
+  }*/
 
 
 
